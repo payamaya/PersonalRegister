86ebe763dd56e84f32c0062290dafa5d397c0bed
payamaya <paulyashou81@gmail.com>
2024-05-20 16:31:08 +0200

    adding some switch case

--- PersonalRegister/Program.cs
+++ PersonalRegister/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Register a new employee: ");
+Console.WriteLine("1. Add employee");
+Console.WriteLine("2. Add Sallary");
+Console.WriteLine("3. Show result");
+Console.WriteLine("4. Exit application");
+
+
+string addNewEmployee = Console.ReadLine();
+
+
+
+    switch (addNewEmployee)
+    {
+        case "1":
+            Console.WriteLine("Please add a name");
+            string name= Console.ReadLine();    
+            Console.WriteLine("Please add a salary");
+           int salary= int.Parse(Console.ReadLine());
+            break;
+
+
+        case "3":
+            Console.WriteLine("Employee info");
+            break;
+
+        default:
+            Console.WriteLine(" Invalid Input");
+            break;
+    }
+ while (addNewEmployee != "4");
+
+Console.WriteLine("Closing application");
+Console.ReadLine();
+//2 krav 
+// ta emot och lagra anställda med namn och lön
+//skriv ut i konsol
